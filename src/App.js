8518348154830593e8995a93770c17c147a62032
Your Name <chaudharyakhil337@gmail.com>
2022-01-30 17:05:59 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,61 +1,11 @@
-import { useState } from "react";
+// import { useState } from "react";
 import "./App.css";
 import LogIn from "./components/LogIn";
 
-function App() {
-  const [values, setValues] = useState({
-    username: "",
-    password: "",
-  });
-
-  const inputs = [
-    {
-      id: 1,
-      name: "username",
-      type: "text",
-      placeholder: "Username",
-      errorMessage: "Please enter the username!",
-      label: "Username",
-    },
-    {
-      id: 2,
-      name: "password",
-      type: "password",
-      placeholder: "Password",
-      errorMessage: "Please enter the password",
-      label: "Password",
-    },
-  ];
-
-  const onChange = (e) => {
-    setValues({ ...values, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
- 
+function App() { 
   return (
     <div className="flex justify-center items-center h-screen bg-gray-100">
-      <form
-        className="bg-white px-14 rounded-lg shadow-md"
-        onSubmit={handleSubmit}
-      >
-        <h1 className="text-pink-500 text-center font-bold text-3xl my-4">
-          Log In
-        </h1>
-        {inputs.map((input) => (
-          <LogIn
-            key={input.id}
-            {...input}
-            value={values[input.name]}
-            onChange={onChange}
-          />
-        ))}
-        <button className="w-full p-3 rounded-md bg-pink-300 hover:bg-pink-400 text-lg font-medium mb-7 mt-3">
-          Submit
-        </button>
-      </form>
+          <LogIn/>
     </div>
   );
 }
--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,18 +1,76 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const LogIn = (props) => {
-  const { label, onChange, errorMessage, id, ...inputProps } = props;
+const LogIn = () => {
+  const [formValues, setFormValues] = useState({
+    username: "",
+    password: "",
+  });
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
 
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormErrors(validate(formValues));
+    setIsSubmit(true);
+  };
+
+  useEffect(() => {
+    console.log(formErrors);
+    if (Object.keys(formErrors).length === 0 && isSubmit) {
+      console.log(formValues);
+    }
+  }, [formErrors]);
+
+  const validate = (values) => {
+    const errors = {};
+    if (!values.username) {
+      errors.username = "Username is required!";
+    }
+    if (!values.password) {
+      errors.password = "Password is required!";
+    }
+    return errors;
+  };
   return (
-    <div className="flex flex-col w-[280px] ">
-      <label className="font-medium text-gray-400">{label}</label>
-      <input
-        className="p-4 my-3 border border-solid border-gray-300 rounded-md"
-        {...inputProps}
-        onChange={onChange}
-      />
-      <span className="text-sm p-1 text-red-600 ">{errorMessage}</span>
-    </div>
+    <form
+      className="bg-white px-14 rounded-lg shadow-md"
+      onSubmit={handleSubmit}
+    >
+      <h1 className="text-pink-500 text-center font-bold text-3xl my-4">
+        Login Form
+      </h1>
+      <div className="flex flex-col w-[280px] ">
+        <label className="font-medium text-gray-400">Username</label>
+        <input
+          className="p-4 my-3 border border-solid border-gray-300 rounded-md"
+          name="username"
+          placeholder="Username"
+          value={formValues.username}
+          onChange={handleChange}
+        />
+        <span className="text-sm p-1 text-red-600 ">{formErrors.username}</span>
+        <label className="font-medium text-gray-400">Password</label>
+        <input
+          className="p-4 my-3 border border-solid border-gray-300 rounded-md"
+          name="password"
+          placeholder="Password"
+          value={formValues.password}
+          onChange={handleChange}
+        />
+        <span className="text-sm p-1 text-red-600 ">{formErrors.password}</span>
+      </div>
+      <button
+        type="submit"
+        className="w-full p-3 rounded-md bg-pink-300 hover:bg-pink-400 text-lg font-medium mb-7 mt-3"
+      >
+        Submit
+      </button>
+    </form>
   );
 };
 
